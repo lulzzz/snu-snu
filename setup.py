6e4e2f411d234fe5eaf2c0cd8398beb6416368f7
Simon Crowe <sc@158.223.71.207>
2017-06-05 13:52:47 +0100

    Changed to using package_dir

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['snusnu'], # Unsure if anything needs to be excluded
+    package_dir={'snusnu' : ''}, # Unsure if anything needs to be excluded
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
