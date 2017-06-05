eaee568bab4231f77bc4495faf446305ec60d6a3
Simon Crowe <sc@158.223.71.207>
2017-06-05 13:09:06 +0100

    Changed contents of py_modules

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=["snusnu"],
+    py_modules=["authentication, headless"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
