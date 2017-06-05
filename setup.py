b91558676c69ba42c72b472865722c87f7d15c0a
Simon Crowe <sc@158.223.71.207>
2017-06-05 12:48:20 +0100

    Attempting to use py_modules list instead of packages

--- setup.py
+++ setup.py
@@ -65,11 +65,11 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(), # Unsure if anythin needs to be excluded
+    #packages=find_packages(), # Unsure if anythin needs to be excluded
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    py_modules=["snusnu"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
