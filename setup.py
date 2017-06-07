f98d658c612869cbf4a5db89343658c72a461e45
simoncrowe <info@simoncrowe.net>
2017-06-07 22:22:17 +0100

    Added pyvirtual sisplay install_requires

--- setup.py
+++ setup.py
@@ -86,7 +86,12 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['selenium', 'nltk', 'requests', 'pillow'],
+    install_requires=[	'selenium', 
+						'nltk', 
+						'requests', 
+						'pillow', 
+						'pyvirtualdisplay'
+					  ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
