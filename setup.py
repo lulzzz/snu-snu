5304b47bd6a0478c89357fed1b92434e7e603c8c
simoncrowe <info@simoncrowe.net>
2017-06-07 21:21:23 +0100

    Added dependencies

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description="A tool for investigating Amazon's recommendation system",
     long_description="""A tool for training Amazon's recommendation
@@ -86,7 +86,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['selenium', 'nltk'],
+    install_requires=['selenium', 'nltk', 'requests', 'pillow'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
